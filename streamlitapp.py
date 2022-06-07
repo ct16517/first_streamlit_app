0de8112bdbea77e3fed1711e084cd41e5db1a35d
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-07 13:20:33 -0400

    Update streamlitapp.py
    Added Header and Breakfast Line items

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinack, & Rocket Smoothie')
+streamlit.text('Hard-boiled Cage-free Egg')
