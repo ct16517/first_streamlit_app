638e01f3e42928e44ce0aad30026741740deb4a0
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-07 14:01:37 -0400

    Update streamlitapp.py
    Added Dataframe "my_fruit_list"
    Added Multiselect Menu
    Updated App to Display

--- streamlitapp.py
+++ streamlitapp.py
@@ -11,3 +11,12 @@ streamlit.text('🐔 Hard-boiled Cage-free Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+#Load DataFrame directly from S3 Bucket
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Generate Selectio Menu
+streamlit.multiselect("Please select your fruit choices: ", list(my_fruit_list.index))
+
+#Display on Screen
+streamlit.dataframe(my_fruit_list)
