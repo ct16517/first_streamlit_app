f2ffc4dd16ada2efbb065f646744ab57bb49d94d
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-07 13:21:59 -0400

    Update streamlitapp.py
    Corrected Breakfast Menu Item typos

--- streamlitapp.py
+++ streamlitapp.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinack, & Rocket Smoothie')
+streamlit.text('Omega-3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach, & Rocket Smoothie')
 streamlit.text('Hard-boiled Cage-free Egg')
