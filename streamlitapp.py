50741db5e3afd5e9126f7b23aa5808d11f082b15
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-07 14:03:52 -0400

    Update streamlitapp.py
    Updated Drop-down display fields

--- streamlitapp.py
+++ streamlitapp.py
@@ -14,6 +14,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Load DataFrame directly from S3 Bucket
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 #Generate Selectio Menu
 streamlit.multiselect("Please select your fruit choices: ", list(my_fruit_list.index))
