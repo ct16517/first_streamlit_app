242cbc234ff2b28cab4cf6c8e80ebb9f021e737c
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-07 14:12:59 -0400

    Update streamlitapp.py
    Added `smart` multi-select menu

--- streamlitapp.py
+++ streamlitapp.py
@@ -16,8 +16,11 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#Generate Selectio Menu
-streamlit.multiselect("Please select your fruit choices: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+#Generate Selection Menu
+fruits_selected = streamlit.multiselect("Please select your fruit choices: ", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+#Generate Sub-set Selection for Display
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display on Screen
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
