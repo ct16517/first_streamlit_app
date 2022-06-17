80d1ba33d2e620dddf4c07f6c01d26ce065d5628
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-17 14:11:47 -0400

    Update streamlitapp.py
    Separated Request search term and  URL

--- streamlitapp.py
+++ streamlitapp.py
@@ -30,8 +30,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 #API_Integration
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 #Format and Display JSON Data in Table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
