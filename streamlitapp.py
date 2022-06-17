cc07a44fd4a6715734967ee2e3f11ff0bb9f5adf
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-17 14:02:10 -0400

    Update streamlitapp.py
    Requests object typo online 30 corrected

--- streamlitapp.py
+++ streamlitapp.py
@@ -27,5 +27,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #API_Integration
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
