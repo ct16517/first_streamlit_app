e0690b43c7211b2a57a8a183e79d85ed5b1ee33d
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-20 09:27:57 -0400

    Update streamlitapp.py
    Adjusting Code Order

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,6 +3,8 @@ import requests
 import pandas
 import snowflake.connector
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -43,8 +45,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #Snowflake Integration
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
