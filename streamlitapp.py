49301bdc521e160f63c293d782dc43fe7c58a403
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-20 09:21:06 -0400

    Update streamlitapp.py
    Added Snowflake interface framework.

--- streamlitapp.py
+++ streamlitapp.py
@@ -3,6 +3,7 @@ import requests
 import pandas
 import snowflake.connector
 
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -40,3 +41,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 #Format and Display JSON Data in Table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+#Snowflake Integration
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
