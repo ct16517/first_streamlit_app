0c4ce4105823355a6735a624368940166ca0bc72
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-20 09:40:50 -0400

    Update streamlitapp.py
    Lowercase applies to execution statement

--- streamlitapp.py
+++ streamlitapp.py
@@ -48,7 +48,7 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 
 #Snowflake Integration
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+my_cur.execute("select * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit list contains:")
 streamlit.text(my_data_row)
