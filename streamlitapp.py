60c03dffb797170a67dd6a5b5ceafbb83dc09b12
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-20 09:28:56 -0400

    Update streamlitapp.py
    bug isolation

--- streamlitapp.py
+++ streamlitapp.py
@@ -4,7 +4,6 @@ import pandas
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -45,6 +44,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #Snowflake Integration
+my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
