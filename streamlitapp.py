537ca927ab61e667dcb1c12f5619ea7a95267533
Cory Talmon <107003235+ct16517@users.noreply.github.com>
2022-06-20 13:35:11 -0400

    Update streamlitapp.py
    revert

--- streamlitapp.py
+++ streamlitapp.py
@@ -59,7 +59,7 @@ my_cur = my_cnx.cursor()
 #streamlit.text("Hello from Snowflake:")
 #streamlit.text(my_data_row)
 
-my_cur.execute("SELECT fruit_name")
+my_cur.execute("select * from fruit_load_list;")
 my_data_row = my_cur.fetchone()
 streamlit.header("The fruit list contains: ")
 streamlit.dataframe(my_data_row)
